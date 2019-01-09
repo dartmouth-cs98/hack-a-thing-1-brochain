2144e30ef541aec4785834f062ab085b6ffd1bfd
annie ke <annieke8@gmail.com>
2019-01-09 14:23:39 -0500

    implemented mining bros

--- brochain.py
+++ brochain.py
@@ -3,12 +3,14 @@ import json
 from time import time
 from uuid import uuid4
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 class Brochain(object):
     def __init__(self):
         self.chain = []
         self.current_bumps = []
+        self.brodes = set()
     
     def new_bro(self, proof, previous_hash=None):
         """
@@ -68,6 +70,13 @@ class Brochain(object):
         """
         return self.chain[-1]
 
+    def register_broode(self, address):
+        """
+        add a new brode
+        """
+        parsed_url = urlparse(address)
+        self.brodes.add(parsed_url.netloc)
+
 
     def proof_of_work(self, last_proof):
         """
@@ -92,8 +101,56 @@ class Brochain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
+    def valid_chain(self, chain):
+        """
+        determine if a given brochain is valid
+        """
+        last_bro = chain[0]
+        current_index = 1
 
-# instantiate node 
+        while current_index < len(chain):
+            bro = chain[current_index]
+            print(f'{last_bro}')
+            print(f'{bro}')
+            print("\n-----------\n")
+            if bro['previous_hash'] != self.hash(last_bro):
+                return False
+            if not self.valid_proof(last_bro['proof'], bro['proof']):
+                return False
+
+            last_bro = bro
+            current_index += 1
+        return True
+
+    def resolve_conflicts(self):
+        """
+        resolve conflicts by replacing with longest chain in network
+        """
+        neighbros = self.brodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for brode in neighbros:
+            response = requests.get(f'http://{brode}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
+
+# instantiate brode 
 # TODO: separate into separate files
 app = Flask(__name__)
 
@@ -104,15 +161,46 @@ brochain = Brochain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "mining a new bro"
+    last_bro = brochain.last_bro
+    last_proof = last_bro['proof']
+    proof = brochain.proof_of_work(last_proof)
 
-@app.route('/transactions/new', methods=['POST'])
+    # reward for finding the proof, always from sender 0
+    # TODO: adjust amount accordingly?
+    brochain.new_transaction(
+        sender="0",
+        recipient=brode_id,
+        amount=1
+    )
+
+    # add bro to chain
+    previous_hash = brochain.hash(last_bro)
+    bro = brochain.new_bro(proof, previous_hash)
+
+    response = {
+        'message': "New Bro Forged",
+        'index': bro['index'],
+        'bumps': bro['bumps'],
+        'proof': bro['proof'],
+        'previous_hash': bro['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/bumps/new', methods=['POST'])
 def new_bump():
-    return "adding a new bump"
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = brochain.new_bump(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
-    reponse = {
+    response = {
         'chain': brochain.chain,
         'length': len(brochain.chain),
     }
@@ -120,3 +208,37 @@ def full_chain():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+@app.route('/brodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    brodes = values.get('brodes')
+    if brodes is None:
+        return "Error: Please supply a valid list of brodes", 400
+
+    for brode in brodes:
+        brochain.register_node(brode)
+
+    response = {
+        'message': 'New brodes have been added',
+        'total_nodes': list(brochain.brodes),
+    }
+    return jsonify(response), 201
+
+@app.route('/brodes/resolve', methods=['GET'])
+def consensus():
+    replaced = brochain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': brochain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': brochain.chain
+        }
+
+    return jsonify(response), 200
